4f228581448455868267199b46a63a6ce01e3354
Dmitry <endeveron@gmail.com>
2021-03-10 10:55:55 +0200

    event type, event handle (opt 1)

--- src/components/ToDoForm/ToDoForm.tsx
+++ src/components/ToDoForm/ToDoForm.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ToDoForm: React.FunctionComponent = () => {
+  const [title, setTitle] = useState<string>('')
+
+  const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value)
+  }
+
+  const keyPressHandler = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter') console.log(title)
+    setTitle('')
+  }
+
   return (
     <div className="input-field mt2">
-      <input type="text" id="title" placeholder="Enter Task Title"/>
-      <label htmlFor="title" className="active">Title</label>
+      <input 
+        value={title}
+        onChange={inputHandler}
+        onKeyPress={keyPressHandler}
+        type="text" 
+        id="title" 
+        placeholder="Enter Task Title"/>
+      <label 
+        htmlFor="title" 
+        className="active">
+          Title
+      </label>
     </div>
   );
 };
